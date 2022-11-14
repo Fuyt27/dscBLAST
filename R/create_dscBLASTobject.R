590197cea80b39bf63016f5ef56a4ff521d25d2c
Fuyt27 <72448711+Fuyt27@users.noreply.github.com>
2022-11-14 11:01:16 +0800

    Add files via upload

--- R/create_dscBLASTobject.R
+++ R/create_dscBLASTobject.R
@@ -1,6 +1,6 @@
 #' Title:create dscBLAST object
 #'
-#' @param expression_profile
+#' @param expression_matrix
 #' cell-gene matrix
 #' @param query_species
 #' options: c('Hs','Mm')
@@ -31,36 +31,36 @@
 #' @export
 #'
 #' @importFrom Seurat PercentageFeatureSet as.SingleCellExperiment CreateSeuratObject SCTransform SplitObject
-create_dscBLASTobject <- function(expression_profile,query_species,ref_species='both',metadata,cell_type,batch='default',downsample=50000,mtx.type='raw') {
+create_dscBLASTobject <- function(expression_matrix,query_species,ref_species='both',metadata,cell_type,batch='default',downsample=50000,mtx.type='raw') {
   if(ref_species!='both'){
-    seob=Cell_qc(expression_profile,query_species,metadata,cell_type,downsample)
+    seob=Cell_qc(expression_matrix,query_species,metadata,cell_type,downsample)
     if(mtx.type=='raw'){
-    message('--step3:SCT transform--')
-    if(batch=='default'){
-      seob<- SCTransform(
-        seob,
-        variable.features.n = 3000,
-        vars.to.regress = c("nCount_RNA", "nFeature_RNA",'percent.mt'),
-        verbose = T)
-    }
-    else{
-    seob_list = SplitObject(seob,split.by = batch)
-    message('--step3:SCT transform--')
-    for(i in 1:length(seob_list)){
-      seob_list[[i]] <- SCTransform(
-        seob_list[[i]],
-        variable.features.n = 3000,
-        vars.to.regress = c("nCount_RNA", "nFeature_RNA",'percent.mt'),
-        verbose = T)
-    }
-    seob = merge(seob_list[[1]],seob_list[-1])}
-    message('--step4:creating dscBLAST object--')
-    seob_sct = CreateSeuratObject(counts = seob@assays[["SCT"]]@data)
-    seob_sct@meta.data=seob@meta.data
-    sce=as.SingleCellExperiment(seob_sct)
-    sce$study_id='Query'
-    return(sce)
-    message('--finished--')}
+      message('--step3:SCT transform--')
+      if(batch=='default'){
+        seob<- SCTransform(
+          seob,
+          variable.features.n = 3000,
+          vars.to.regress = c("nCount_RNA", "nFeature_RNA",'percent.mt'),
+          verbose = T)
+      }
+      else{
+        seob_list = SplitObject(seob,split.by = batch)
+        message('--step3:SCT transform--')
+        for(i in 1:length(seob_list)){
+          seob_list[[i]] <- SCTransform(
+            seob_list[[i]],
+            variable.features.n = 3000,
+            vars.to.regress = c("nCount_RNA", "nFeature_RNA",'percent.mt'),
+            verbose = T)
+        }
+        seob = merge(seob_list[[1]],seob_list[-1])}
+      message('--step4:creating dscBLAST object--')
+      seob_sct = CreateSeuratObject(counts = seob@assays[["SCT"]]@data)
+      seob_sct@meta.data=seob@meta.data
+      sce=as.SingleCellExperiment(seob_sct)
+      sce$study_id='Query'
+      return(sce)
+      message('--finished--')}
     else if (mtx.type=='normalized'){
       message('--step3:creating dscBLAST object--')
       seob_prenorm= CreateSeuratObject(counts = seob@assays[["RNA"]]@data)
@@ -69,43 +69,43 @@ create_dscBLASTobject <- function(expression_profile,query_species,ref_species='
       sce$study_id='Query'
       return(sce)
     }
-    }
+  }
   else{
-    seob=Cell_qc(expression_profile,query_species,metadata,cell_type,downsample)
+    seob=Cell_qc(expression_matrix,query_species,metadata,cell_type,downsample)
     if(mtx.type=='raw'){
-    if(batch=='default'){
-      message('--step3:SCT transform--')
-      seob<- SCTransform(
-        seob,
-        variable.features.n = 3000,
-        vars.to.regress = c("nCount_RNA", "nFeature_RNA",'percent.mt'),
-        verbose = T)
-    }
-    else{
-      seob_list = SplitObject(seob,split.by = batch)
-      message('--step3:SCT transform--')
-      for(i in 1:length(seob_list)){
-        seob_list[[i]] <- SCTransform(
-          seob_list[[i]],
+      if(batch=='default'){
+        message('--step3:SCT transform--')
+        seob<- SCTransform(
+          seob,
           variable.features.n = 3000,
           vars.to.regress = c("nCount_RNA", "nFeature_RNA",'percent.mt'),
           verbose = T)
       }
-      seob = merge(seob_list[[1]],seob_list[-1])}
-    message('--step4:creating dscBLAST object--')
-    counts = seob@assays[["SCT"]]@data
-    seob_sct = CreateSeuratObject(counts = counts)
-    seob_sct@meta.data=seob@meta.data
-    sce1=as.SingleCellExperiment(seob_sct)
-    sce1$study_id='Query'
-
-    counts2<-transfer_gene(counts,query_species)
-    seob_sct = CreateSeuratObject(counts = counts2)
-    seob_sct@meta.data=seob@meta.data
-    sce2=as.SingleCellExperiment(seob_sct)
-    sce2$study_id='Query'
-    sce_list=list(sce1,sce2);names(sce_list)=c(query_species,ifelse(query_species == "Hs", 'Mm', 'Hs'))
-    return(sce_list)}
+      else{
+        seob_list = SplitObject(seob,split.by = batch)
+        message('--step3:SCT transform--')
+        for(i in 1:length(seob_list)){
+          seob_list[[i]] <- SCTransform(
+            seob_list[[i]],
+            variable.features.n = 3000,
+            vars.to.regress = c("nCount_RNA", "nFeature_RNA",'percent.mt'),
+            verbose = T)
+        }
+        seob = merge(seob_list[[1]],seob_list[-1])}
+      message('--step4:creating dscBLAST object--')
+      counts = seob@assays[["SCT"]]@data
+      seob_sct = CreateSeuratObject(counts = counts)
+      seob_sct@meta.data=seob@meta.data
+      sce1=as.SingleCellExperiment(seob_sct)
+      sce1$study_id='Query'
+      
+      counts2<-transfer_gene(counts,query_species)
+      seob_sct = CreateSeuratObject(counts = counts2)
+      seob_sct@meta.data=seob@meta.data
+      sce2=as.SingleCellExperiment(seob_sct)
+      sce2$study_id='Query'
+      sce_list=list(sce1,sce2);names(sce_list)=c(query_species,ifelse(query_species == "Hs", 'Mm', 'Hs'))
+      return(sce_list)}
     else if(mtx.type=='normalized'){
       message('--step3:creating dscBLAST object--')
       counts = seob@assays[["RNA"]]@data
@@ -113,7 +113,7 @@ create_dscBLASTobject <- function(expression_profile,query_species,ref_species='
       seob_prenorm@meta.data=seob@meta.data
       sce1=as.SingleCellExperiment(seob_prenorm)
       sce1$study_id='Query'
-
+      
       counts2<-transfer_gene(counts,query_species)
       seob_prenorm = CreateSeuratObject(counts = counts2)
       seob_prenorm@meta.data=seob@meta.data
@@ -121,7 +121,7 @@ create_dscBLASTobject <- function(expression_profile,query_species,ref_species='
       sce2$study_id='Query'
       sce_list=list(sce1,sce2);names(sce_list)=c(query_species,ifelse(query_species == "Hs", 'Mm', 'Hs'))
       return(sce_list)
-
+      
     }
     message('--finished--')
   }}
